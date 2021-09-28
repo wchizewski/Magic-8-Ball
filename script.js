9f2688c6f984045d2de2d1b84b0a9a5c93cf0b70
wchizewski <w.chizewski@share.epsb.ca>
2021-09-28 13:42:45 -0600

    finished 8 ball

--- script.js
+++ script.js
@@ -14,25 +14,28 @@ function answer() {
     // case insensitive
     let question = quesInEl.value.toLowerCase();
     console.log(question)
-    // pick answer
-    let randAns = Math.random();
+    // check input
     if (question.length === 0) {
         answerOutEl.innerHTML = "Please ask a question..."
     } else if (question === "is javascipt awesome" || question === "is javascript awesome?") {
         answerOutEl.innerHTML = "Of course!"
     } else if (question === "does a magic 8 ball actually work" || question === "does a magic 8 ball actually work?") {
         answerOutEl.innerHTML = "How dare you doubt me!"
-    } else if (question === "what is your favorite color" || question === "what is your favorite color?") {
+    } else if (question === "what is your favorite color" || question === "what is your favorite color?" || question === "what's your favorite color" || question === "what's your favorite color?") {
         answerOutEl.innerHTML = "Black and white, because I'm an 8 ball!"
-    } else if (randAns < 0.20) {
-        answerOutEl.innerHTML = "Without a doubt."
-    } else if (randAns < 0.40) {
-        answerOutEl.innerHTML = "As I see it, yes."
-    } else if (randAns < 0.60) {
-        answerOutEl.innerHTML = "Concentrate and ask again."
-    } else if (randAns < 0.80) {
-        answerOutEl.innerHTML = "Don't count on it."
     } else {
-        answerOutEl.innerHTML = "Outlook not so good."
+        // pick random answer
+        let randAns = Math.random();
+        if (randAns < 0.20) {
+            answerOutEl.innerHTML = "Without a doubt."
+        } else if (randAns < 0.40) {
+            answerOutEl.innerHTML = "As I see it, yes."
+        } else if (randAns < 0.60) {
+            answerOutEl.innerHTML = "Concentrate and ask again."
+        } else if (randAns < 0.80) {
+            answerOutEl.innerHTML = "Don't count on it."
+        } else {
+            answerOutEl.innerHTML = "Outlook not so good."
+        }
     }
 }
